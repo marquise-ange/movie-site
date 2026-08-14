6b8d9f0ab7bb18c42ecadba27a0348709a969915
Ange Marquise <angemarquisei@gmail.com>
2026-08-15 00:47:26 +0200

    styling

--- movie-site/src/App.jsx
+++ movie-site/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import Home from "./pages/Home.jsx"
 import Favorites from "./pages/Favorites.jsx"
-import Navbar from "./Components/Navbar.jsx"
 import { Route, Routes } from 'react-router-dom'//importing the route and routes from react-router-dom to use the routing functionality in the app.
+import Navbar from "./Components/Navbar"
 
 // a component is a function in javascript that returns JSX (HTML in JS)(Jsx means JavaScript with html)
 //(this is a fragment, it is used to wrap multiple elements without adding an extra node to the DOM(<></>))
