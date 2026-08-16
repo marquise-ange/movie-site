4618d5cc8011a3b70dbc433fae2946412e7cd823
Ange Marquise <angemarquisei@gmail.com>
2026-08-16 23:26:08 +0200

    fetching

--- movie-site/src/services/Api.js
+++ movie-site/src/services/Api.js
@@ -1,2 +1,15 @@
 const API_KEY = "8b65600887348a563d84dd643a456135";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
